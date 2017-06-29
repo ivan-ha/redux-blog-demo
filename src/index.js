dda69079d3c912c94cb125764cec130010fdbdc3
Ivan HA <hangolam@gmail.com>
2017-06-30 00:56:34 +0800

    PostsIndex with title rendered

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import {BrowserRouter, Route} from 'react-router-dom';
+import ReduxPromise from 'redux-promise';
 
 import reducer from './reducers';
 import PostsIndex from './components/posts_index';
 
-const store = createStore(reducer, applyMiddleware());
+const store = createStore(reducer, applyMiddleware(ReduxPromise));
 
 const AppContainter = () => {
     return (
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import {combineReducers} from 'redux';
+import PostsReducer from './reducer_posts';
 
 const rootReducer = combineReducers({
-    state: () => ({})
+    posts: PostsReducer
 });
 
 export default rootReducer;
