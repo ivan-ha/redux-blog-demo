a5acfd0463bb6f577e5a78e34970f80ac45db9b6
Ivan HA <hangolam@gmail.com>
2017-06-30 01:09:28 +0800

    create PostsNew component and handle its route

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import ReduxPromise from 'redux-promise';
 
 import reducer from './reducers';
 import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
 const store = createStore(reducer, applyMiddleware(ReduxPromise));
 
@@ -14,9 +15,10 @@ const AppContainter = () => {
     return (
         <Provider store={store}>
             <BrowserRouter>
-                <div>
+                <Switch>
+                    <Route path="/posts/new" component={PostsNew} />
                     <Route path="/" component={PostsIndex} />
-                </div>
+                </Switch>
             </BrowserRouter>
         </Provider>
     );
