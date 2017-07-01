a77f970c749255b8cf72bcbfa142af2e12885999
Ivan HA <hangolam@gmail.com>
2017-07-02 02:06:30 +0800

    render post detail in new page

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import ReduxPromise from 'redux-promise';
 import reducer from './reducers';
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 
 const store = createStore(reducer, applyMiddleware(ReduxPromise));
 
@@ -17,6 +18,7 @@ const AppContainter = () => {
             <BrowserRouter>
                 <Switch>
                     <Route path="/posts/new" component={PostsNew} />
+                    <Route path="/posts/:id" component={PostsShow} />
                     <Route path="/" component={PostsIndex} />
                 </Switch>
             </BrowserRouter>
